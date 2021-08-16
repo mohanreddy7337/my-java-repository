4068b3ac9211403effe70226daef982109f9f3c3
mohan <mohanreddy7337@gmail.com>
2021-08-16 12:14:57 +0530

    my second commit

--- Second.java
+++ Second.java
@@ -2,6 +2,13 @@ class Second
 {
   public static void main(String[] args)
   {
+
    System.out.println("my second code");
+   int i=1;
+   while(i<=2)
+  {
+   System.out.println("This is while loop");
+   i++
+   }
   }
 }
